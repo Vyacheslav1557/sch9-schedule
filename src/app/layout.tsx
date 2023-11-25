5994d62ee6bc3873f02894b325d6e65ae9a08480
HomePC <slavabirin1@yandex.ru>
2023-11-25 20:06:55 +0500

    fix: delete metadata description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
+import React from 'react';
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 import "@src/app-flat/styles/index.css"
-import React from 'react';
 import {MantineProvider} from '@mantine/core';
 
 export const metadata = {
     title: 'Schedule',
-    description: 'I am using Mantine with Next.js!',
 };
 
 export default function RootLayout({children}: { children: any }) {
